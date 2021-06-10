ca591f63bcc30160cec9cd5191d7494f0df434b5
Joons <elddy0948@gmail.com>
2021-06-10 16:53:17 +0900

    [Add➕] : User Model 생성

---
+++ NLetterPoem/NLetterPoem/Models/User.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+class User {
+    var email: String
+    var password: String
+    var profilePhotoURL: String
+    var nickname: String
+    var bio: String
+    var firstPlaceCount: Int
+    var secondPlaceCount: Int
+    var thirdPlaceCount: Int
+    var participationCount: Int
+    
+    init(email: String, password: String, profilePhotoURL: String, nickname: String, bio: String,
+         firstPlaceCount: Int = 0, secondPlaceCount: Int = 0, thirdPlaceCount: Int = 0, participationCount: Int = 0) {
+        self.email = email
+        self.password = password
+        self.profilePhotoURL = profilePhotoURL
+        self.nickname = nickname
+        self.bio = bio
+        self.firstPlaceCount = firstPlaceCount
+        self.secondPlaceCount = secondPlaceCount
+        self.thirdPlaceCount = thirdPlaceCount
+        self.participationCount = participationCount
+    }
+}

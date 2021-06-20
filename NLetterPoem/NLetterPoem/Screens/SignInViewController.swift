dacd43a29d1d63386ae455dcf27abc1a4bfb9192
Joons <elddy0948@gmail.com>
2021-06-20 14:00:36 +0900

    [Feat💡] : CustomTextField 코드 작성

--- NLetterPoem/NLetterPoem/Screens/SignInViewController.swift
+++ NLetterPoem/NLetterPoem/Screens/SignInViewController.swift
@@ -1,13 +1,49 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
+    //MARK: - Views
+    private var signinStackView: UIStackView!
+    private(set) var emailTextField: NLPTextField!
+    private(set) var passwordTextField: NLPTextField!
+    
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSignInStackView()
         configure()
+        configureLayout()
     }
     
     private func configure() {
-        view.backgroundColor = .systemYellow
+        view.addSubview(signinStackView)
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "로그인"
+    }
+    
+    private func configureSignInStackView() {
+        emailTextField = NLPTextField(type: .email)
+        passwordTextField = NLPTextField(type: .password)
+        
+        signinStackView = UIStackView()
+        signinStackView.distribution = .equalSpacing
+        signinStackView.axis = .vertical
+        signinStackView.translatesAutoresizingMaskIntoConstraints = false
+        signinStackView.spacing = 8
+        
+        signinStackView.addArrangedSubview(emailTextField)
+        signinStackView.addArrangedSubview(passwordTextField)
+    }
+    
+    private func configureLayout() {
+        let padding: CGFloat = 8
+        NSLayoutConstraint.activate([
+            signinStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            signinStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            signinStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 52),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 52)
+        ])
     }
 }
---
+++ NLetterPoem/NLetterPoem/Views/CustomViews/NLPTextField.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class NLPTextField: UITextField {
+    //MARK: - enum FieldType
+    enum FieldType: String {
+        case email = "Email"
+        case password = "Password"
+        case normal = "Normal"
+    }
+    
+    //MARK: - Properties
+    var textFieldType: FieldType!
+    
+    
+    //MARK: - Initializer
+    init(type: FieldType) {
+        textFieldType = type
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Privates
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .secondarySystemBackground
+        
+        layer.cornerRadius = 16
+        layer.masksToBounds = true
+        layer.borderWidth = 3
+        layer.borderColor = UIColor.systemGray.cgColor
+        
+        autocorrectionType = .no
+        autocapitalizationType = .none
+        
+        leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        leftViewMode = .always
+    }
+}

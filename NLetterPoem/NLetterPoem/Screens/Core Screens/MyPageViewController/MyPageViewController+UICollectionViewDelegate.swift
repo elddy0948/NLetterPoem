8fbfc87456ddee64bb161b82eecda7bf763a50f7
Joons <elddy0948@gmail.com>
2021-08-12 16:33:24 +0900

    [Feat💡] : PoemDetailViewController에 작성자 이름을 누를시 기능 추가
    작성자 이름을 누르면 해당 작성자의 마이페이지로 이동.

--- NLetterPoem/NLetterPoem/Screens/Core Screens/MyPageViewController/MyPageViewController+UICollectionViewDelegate.swift
+++ NLetterPoem/NLetterPoem/Screens/Core Screens/MyPageViewController/MyPageViewController+UICollectionViewDelegate.swift
@@ -1,11 +1,12 @@
 import UIKit
 
 extension MyPageViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let poem = poems?[indexPath.item] {
-            let vc = PoemDetailViewController()
-            vc.poem = poem
-            navigationController?.pushViewController(vc, animated: true)
-        }
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    if let poem = poems?[indexPath.item] {
+      let vc = PoemDetailViewController()
+      vc.poem = poem
+      vc.enableAuthorButton = false
+      navigationController?.pushViewController(vc, animated: true)
     }
+  }
 }
--- NLetterPoem/NLetterPoem/Screens/Others/PoemDetailViewController.swift
+++ NLetterPoem/NLetterPoem/Screens/Others/PoemDetailViewController.swift
@@ -2,198 +2,215 @@ import UIKit
 import Firebase
 
 final class PoemDetailViewController: UIViewController {
-    
-    private(set) var detailPoemView: DetailPoemView?
-    
-    //MARK: - Properties
-    var poem: NLPPoem? {
-        didSet {
-            self.detailPoemView?.updatePoem(with: poem)
-        }
+  
+  private(set) var detailPoemView: DetailPoemView?
+  
+  //MARK: - Properties
+  var poem: NLPPoem? {
+    didSet {
+      self.detailPoemView?.updatePoem(with: poem)
     }
-    var fireState = false
-    var currentUser: NLPUser? {
-        didSet {
-            self.configurePoemDetailView()
-        }
+  }
+  var fireState = false
+  var currentUser: NLPUser? {
+    didSet {
+      self.configurePoemDetailView()
     }
-    var handler: AuthStateDidChangeListenerHandle?
+  }
+  var handler: AuthStateDidChangeListenerHandle?
+  var enableAuthorButton = true
+  
+  //MARK: - View Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    handler = Auth.auth().addStateDidChangeListener({ [weak self] auth, user in
+      guard let self = self,
+            let user = user,
+            let email = user.email else {
+        return
+      }
+      self.configureUser(email: email)
+    })
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    if let handler = handler {
+      Auth.auth().removeStateDidChangeListener(handler)
+    }
+  }
+  
+  //MARK: - Configure Logic
+  private func configurePoemDetailView() {
+    guard let poem = poem,
+          let user = currentUser else { return }
     
-    //MARK: - View Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        handler = Auth.auth().addStateDidChangeListener({ [weak self] auth, user in
-            guard let self = self,
-                  let user = user,
-                  let email = user.email else {
-                return
-            }
-            self.configureUser(email: email)
-        })
+    user.likedPoem.contains(poem.id) ? (fireState = true) : (fireState = false)
+    
+    detailPoemView = DetailPoemView(poem: poem,
+                                    fireState: fireState,
+                                    enableAuthorButton: enableAuthorButton)
+    detailPoemView?.delegate = self
+    
+    guard let detailPoemView = detailPoemView else { return }
+    view.addSubview(detailPoemView)
+    
+    NSLayoutConstraint.activate([
+      detailPoemView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      detailPoemView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      detailPoemView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      detailPoemView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+    ])
+    
+    //If visitor is poem's author can show 'Edit' button
+    if poem.authorEmail == user.email {
+      configureRightBarButtonItem()
+    }
+  }
+  
+  private func configureUser(email: String) {
+    UserDatabaseManager.shared.fetchUserInfo(with: email) { [weak self] user in
+      guard let self = self,
+            let user = user else {
+        return
+      }
+      self.currentUser = user
+    }
+  }
+  
+  //MARK: - UI Logic
+  private func configureRightBarButtonItem() {
+    let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit,
+                                             target: self,
+                                             action: #selector(editButtonAction(_:)))
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+  }
+  
+  
+  //MARK: - LikeCount Logic
+  private func updateLikeCount(id: String, authorEmail: String, isIncrease: Bool) {
+    PoemDatabaseManager.shared.updatePoemLikeCount(id: id,
+                                                   authorEmail: authorEmail,
+                                                   isIncrease: isIncrease)
+  }
+  
+  private func updateUserLikedPoem(email: String, id: String, isRemove: Bool) {
+    if isRemove {
+      UserDatabaseManager.shared.removeLikedPoem(userEmail: email,
+                                                 poemID: id)
+    } else {
+      UserDatabaseManager.shared.addLikedPoem(userEmail: email,
+                                              poemID: id)
+    }
+  }
+  
+  //MARK: -  Actions
+  @objc func editButtonAction(_ sender: UIBarButtonItem) {
+    let alertController = configureAlertController()
+    present(alertController, animated: true, completion: nil)
+  }
+  
+  private func configureAlertController() -> UIAlertController {
+    let alertController = UIAlertController(title: nil,
+                                            message: nil,
+                                            preferredStyle: .actionSheet)
+    let editAction = UIAlertAction(title: "수정",
+                                   style: .default) { [weak self] action in
+      guard let self = self else { return }
+      let viewController = self.configureCreatePoemViewController()
+      self.present(viewController, animated: true, completion: nil)
+    }
+    let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { [weak self] action in
+      guard let self = self else { return }
+      //TODO: - Delete Action
+      self.deletePoem()
+      self.navigationController?.popToRootViewController(animated: true)
+    }
+    let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in }
+    
+    alertController.addAction(editAction)
+    alertController.addAction(deleteAction)
+    alertController.addAction(cancelAction)
+    
+    return alertController
+  }
+  
+  private func configureCreatePoemViewController() -> CreatePoemViewController {
+    let viewController = CreatePoemViewController()
+    viewController.action = .edit
+    viewController.editPoem = self.poem
+    viewController.topic = self.poem?.topic
+    viewController.delegate = self
+    return viewController
+  }
+  
+  private func deletePoem() {
+    guard let poem = poem,
+          let currentUser = currentUser else { return }
+    PoemDatabaseManager.shared.deletePoem(poem,
+                                          requester: currentUser.email) { result in
+      switch result {
+      case .success(let message):
+        print(message)
+      case .failure(let error):
+        print(error.message)
+      }
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        if let handler = handler {
-            Auth.auth().removeStateDidChangeListener(handler)
-        }
-    }
-    
-    //MARK: - Configure Logic
-    private func configurePoemDetailView() {
-        guard let poem = poem,
-              let user = currentUser else { return }
-        
-        user.likedPoem.contains(poem.id) ? (fireState = true) : (fireState = false)
-        
-        detailPoemView = DetailPoemView(poem: poem,
-                                        fireState: fireState)
-        detailPoemView?.delegate = self
-        
-        guard let detailPoemView = detailPoemView else { return }
-        view.addSubview(detailPoemView)
-        
-        NSLayoutConstraint.activate([
-            detailPoemView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            detailPoemView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            detailPoemView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            detailPoemView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-        ])
-        
-        //If visitor is poem's author can show 'Edit' button
-        if poem.authorEmail == user.email {
-            configureRightBarButtonItem()
-        }
-    }
-    
-    private func configureUser(email: String) {
-        UserDatabaseManager.shared.fetchUserInfo(with: email) { [weak self] user in
-            guard let self = self,
-                  let user = user else {
-                return
-            }
-            self.currentUser = user
-        }
-    }
-    
-    //MARK: - UI Logic
-    private func configureRightBarButtonItem() {
-        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit,
-                                                 target: self,
-                                                 action: #selector(editButtonAction(_:)))
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.navigationItem.rightBarButtonItem = rightBarButtonItem
-        }
-    }
-    
-    
-    //MARK: - LikeCount Logic
-    private func updateLikeCount(id: String, authorEmail: String, isIncrease: Bool) {
-        PoemDatabaseManager.shared.updatePoemLikeCount(id: id,
-                                                       authorEmail: authorEmail,
-                                                       isIncrease: isIncrease)
-    }
-    
-    private func updateUserLikedPoem(email: String, id: String, isRemove: Bool) {
-        if isRemove {
-            UserDatabaseManager.shared.removeLikedPoem(userEmail: email,
-                                                       poemID: id)
-        } else {
-            UserDatabaseManager.shared.addLikedPoem(userEmail: email,
-                                                    poemID: id)
-        }
-    }
-    
-    //MARK: -  Actions
-    @objc func editButtonAction(_ sender: UIBarButtonItem) {
-        let alertController = configureAlertController()
-        present(alertController, animated: true, completion: nil)
-    }
-    
-    private func configureAlertController() -> UIAlertController {
-        let alertController = UIAlertController(title: nil,
-                                                message: nil,
-                                                preferredStyle: .actionSheet)
-        let editAction = UIAlertAction(title: "수정",
-                                       style: .default) { [weak self] action in
-            guard let self = self else { return }
-            let viewController = self.configureCreatePoemViewController()
-            self.present(viewController, animated: true, completion: nil)
-        }
-        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { [weak self] action in
-            guard let self = self else { return }
-            //TODO: - Delete Action
-            self.deletePoem()
-            self.navigationController?.popToRootViewController(animated: true)
-        }
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in }
-        
-        alertController.addAction(editAction)
-        alertController.addAction(deleteAction)
-        alertController.addAction(cancelAction)
-        
-        return alertController
-    }
-    
-    private func configureCreatePoemViewController() -> CreatePoemViewController {
-        let viewController = CreatePoemViewController()
-        viewController.action = .edit
-        viewController.editPoem = self.poem
-        viewController.topic = self.poem?.topic
-        viewController.delegate = self
-        return viewController
-    }
-    
-    private func deletePoem() {
-        guard let poem = poem,
-              let currentUser = currentUser else { return }
-        PoemDatabaseManager.shared.deletePoem(poem,
-                                              requester: currentUser.email) { result in
-            switch result {
-            case .success(let message):
-                print(message)
-            case .failure(let error):
-                print(error.message)
-            }
-        }
-        
-        UserDatabaseManager.shared.removePoem(userEmail: currentUser.email,
-                                              poemID: poem.id)
-    }
+    UserDatabaseManager.shared.removePoem(userEmail: currentUser.email,
+                                          poemID: poem.id)
+  }
 }
 
 extension PoemDetailViewController: DetailPoemViewDelegate {
-    func didTappedFireButton(_ detailPoemView: DetailPoemView, _ fireButton: UIButton) {
-        guard let user = currentUser,
-              let poem = poem else { return }
-        
-        fireState.toggle()
-        
-        if fireState {
-            updateLikeCount(id: poem.id,
-                            authorEmail: poem.authorEmail,
-                            isIncrease: true)
-            updateUserLikedPoem(email: user.email,
-                                id: poem.id,
-                                isRemove: false)
-        } else {
-            updateLikeCount(id: poem.id,
-                            authorEmail: poem.authorEmail,
-                            isIncrease: false)
-            updateUserLikedPoem(email: user.email,
-                                id: poem.id,
-                                isRemove: true)
-        }
-        
-        fireButton.isSelected = fireState
-        fireState ? (fireButton.tintColor = .systemRed) : (fireButton.tintColor = .label)
+  func detailPoemView(_ view: DetailPoemView,
+                      didTapAuthor author: String?) {
+    guard let email = poem?.authorEmail else { return }
+    let viewController = MyPageViewController()
+    
+    UserDatabaseManager.shared.fetchUserInfo(with: email) { [weak self] user in
+      guard let self = self,
+            let user = user else { return }
+      viewController.user = user
+      self.navigationController?.pushViewController(viewController, animated: true)
     }
+    
+  }
+  
+  func didTappedFireButton(_ detailPoemView: DetailPoemView,
+                           _ fireButton: UIButton) {
+    guard let user = currentUser,
+          let poem = poem else { return }
+    
+    fireState.toggle()
+    
+    if fireState {
+      updateLikeCount(id: poem.id,
+                      authorEmail: poem.authorEmail,
+                      isIncrease: true)
+      updateUserLikedPoem(email: user.email,
+                          id: poem.id,
+                          isRemove: false)
+    } else {
+      updateLikeCount(id: poem.id,
+                      authorEmail: poem.authorEmail,
+                      isIncrease: false)
+      updateUserLikedPoem(email: user.email,
+                          id: poem.id,
+                          isRemove: true)
+    }
+    
+    fireButton.isSelected = fireState
+    fireState ? (fireButton.tintColor = .systemRed) : (fireButton.tintColor = .label)
+  }
 }
 
 extension PoemDetailViewController: CreatePoemViewControllerDelegate {
-    func createPoemViewController(_ viewController: CreatePoemViewController, didTapDone poem: NLPPoem) {
-        self.poem = poem
-    }
+  func createPoemViewController(_ viewController: CreatePoemViewController, didTapDone poem: NLPPoem) {
+    self.poem = poem
+  }
 }

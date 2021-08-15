a6c391b494dce94b955210487e3943bac877c510
Joons <elddy0948@gmail.com>
2021-08-15 14:15:32 +0900

    [Style✏️] : 라인 정리

--- NLetterPoem/NLetterPoem/Screens/Core Screens/MyPageViewController/MyPageViewController+UICollectionViewDataSource.swift
+++ NLetterPoem/NLetterPoem/Screens/Core Screens/MyPageViewController/MyPageViewController+UICollectionViewDataSource.swift
@@ -1,50 +1,56 @@
 import UIKit
 
 extension MyPageViewController: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      numberOfItemsInSection section: Int) -> Int {
+    return user?.poems.count ?? 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MyPageCollectionViewCell.reuseIdentifier,
+            for: indexPath) as? MyPageCollectionViewCell else {
+      return UICollectionViewCell()
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return user?.poems.count ?? 0
+    if let poems = poems,
+       indexPath.item <= poems.count {
+      let poem = poems[indexPath.item]
+      cell.setPoemData(with: poem)
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyPageCollectionViewCell.reuseIdentifier, for: indexPath) as? MyPageCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        if let poems = poems,
-           indexPath.item <= poems.count {
-            let poem = poems[indexPath.item]
-            cell.setPoemData(with: poem)
-        }
-
-        return cell
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      viewForSupplementaryElementOfKind kind: String,
+                      at indexPath: IndexPath) -> UICollectionReusableView {
+    switch kind {
+    case UICollectionView.elementKindSectionHeader:
+      let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                       withReuseIdentifier: MyPageHeaderView.reuseIdentifier,
+                                                                       for: indexPath)
+      guard let typeHeaderView = headerView as? MyPageHeaderView else { return headerView }
+      let rootViewController = navigationController?.viewControllers.first
+      
+      if rootViewController is MyPageViewController {
+        typeHeaderView.configureUser(with: user,
+                                     isEditButtonHidden: false)
+      } else {
+        typeHeaderView.configureUser(with: user,
+                                     isEditButtonHidden: true)
+      }
+      
+      typeHeaderView.delegate = self
+      
+      return typeHeaderView
+    default:
+      assert(false)
     }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
-        case UICollectionView.elementKindSectionHeader:
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
-                                                                             withReuseIdentifier: MyPageHeaderView.reuseIdentifier,
-                                                                             for: indexPath)
-            guard let typeHeaderView = headerView as? MyPageHeaderView else { return headerView }
-            let rootViewController = navigationController?.viewControllers.first
-            
-            if rootViewController is MyPageViewController {
-                typeHeaderView.configureUser(with: user,
-                                             isEditButtonHidden: false)
-            } else {
-                typeHeaderView.configureUser(with: user,
-                                             isEditButtonHidden: true)
-            }
-          
-            typeHeaderView.delegate = self
-            
-            return typeHeaderView
-        default:
-            assert(false)
-        }
-    }
+  }
 }

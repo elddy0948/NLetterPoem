c4fd5c928342f3948829fa568b6b37f0da4fe1c3
Joons <elddy0948@gmail.com>
2021-09-23 17:51:10 +0900

    [Feat💡] : NLPUser에 BlockedUser 추가

--- NLetterPoem/NLetterPoem/Models/NLPUser.swift
+++ NLetterPoem/NLetterPoem/Models/NLPUser.swift
@@ -1,19 +1,20 @@
 import Foundation
 
 final class NLPUser: Codable {
-    var email: String
-    var profilePhotoURL: String
-    var nickname: String
-    var bio: String
-    var fires: Int = 0
-    var participationCount: Int = 0
-    var poems: [String] = []
-    var likedPoem: [String] = []
-    
-    init(email: String, profilePhotoURL: String, nickname: String, bio: String) {
-        self.email = email
-        self.profilePhotoURL = profilePhotoURL
-        self.nickname = nickname
-        self.bio = bio
-    }
+  var email: String
+  var profilePhotoURL: String
+  var nickname: String
+  var bio: String
+  var fires: Int = 0
+  var participationCount: Int = 0
+  var poems: [String] = []
+  var likedPoem: [String] = []
+  var blockedUser: [String] = []
+  
+  init(email: String, profilePhotoURL: String, nickname: String, bio: String) {
+    self.email = email
+    self.profilePhotoURL = profilePhotoURL
+    self.nickname = nickname
+    self.bio = bio
+  }
 }

0f8ac720b500285a0b289e870b57905519064ec7
Joons <elddy0948@gmail.com>
2021-10-05 17:03:29 +0900

    [Feat💡] : HomeTableView init

--- NLetterPoem/NLetterPoem/Screens/Core Screens/HomeViewcontroller/HotViewController/HotViewController.swift
+++ NLetterPoem/NLetterPoem/Screens/Core Screens/HomeViewcontroller/HotViewController/HotViewController.swift
@@ -1,18 +1,46 @@
-//
-//  HotViewController.swift
-//  NLetterPoem
-//
-//  Created by 김호준 on 2021/10/03.
-//
-
 import UIKit
 
-class HotViewController: UIViewController {
+final class HotViewController: UIViewController {
+  
+  private var homeTableView = HomeTableView()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    
+  }
+  private func configure() {
+    homeTableView = HomeTableView()
+    view = homeTableView
+    
+    homeTableView.backgroundColor = .systemBackground
+    homeTableView.delegate = self
+    homeTableView.dataSource = self
+    homeTableView.homeTableViewDelegate = self
+    
+    homeTableView.register(HomeTableViewCell.self,
+                           forCellReuseIdentifier: HomeTableViewCell.reuseIdentifier)
+    homeTableView.register(HomeEmptyCell.self,
+                           forCellReuseIdentifier: HomeEmptyCell.reuseIdentifier)
+    navigationController?.navigationBar.tintColor = .label
+  }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      view.backgroundColor = .systemTeal
-    }
+extension HotViewController: UITableViewDelegate {
+  
+}
+extension HotViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
 
+}
 
+extension HotViewController: HomeTableViewDelegate {
+  func handleRefreshHomeTableView(_ tableView: HomeTableView) {
+  }
 }

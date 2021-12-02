e04c46f7238976adf20372e58f773295aced708d
Joons <elddy0948@gmail.com>
2021-12-02 14:56:23 +0900

    [Refactor🛠] : PoemViewModel을 활용하여 SearchViewModel 리팩토링

--- NLetterPoem/NLetterPoem/Screens/Core Screens/SearchScreen/SearchViewModel.swift
+++ NLetterPoem/NLetterPoem/Screens/Core Screens/SearchScreen/SearchViewModel.swift
@@ -7,15 +7,18 @@ class SearchViewModel {
   
   private let reference = Firestore.firestore().collection("poems")
   
-  func search(topic: String) -> Observable<[NLPPoem]> {
+  func search(topic: String) -> Observable<[PoemViewModel]> {
     reference.whereField("topic", isEqualTo: topic).rx
       .getDocuments().map({ querySnapshot in
         querySnapshot.documents.map({ document in
           do {
             let poem = try document.data(as: NLPPoem.self)
-            return poem ?? .emptyPoem()
+            let poemViewModel = PoemViewModel(
+              poem ?? .emptyPoem()
+            )
+            return poemViewModel
           } catch {
-            return .emptyPoem()
+            return PoemViewModel(.emptyPoem())
           }
         })
       })

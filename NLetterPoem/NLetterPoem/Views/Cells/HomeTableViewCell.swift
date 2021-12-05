2dfbac6d0b9a2af9c18b34a2740b030008ec27e9
Joons <elddy0948@gmail.com>
2021-12-05 23:57:06 +0900

    [Refactor🛠] : StackView를 활용하여 불필요한 레이아웃 설정들 없애기

--- NLetterPoem/NLetterPoem/Views/Cells/HomeTableViewCell.swift
+++ NLetterPoem/NLetterPoem/Views/Cells/HomeTableViewCell.swift
@@ -6,17 +6,50 @@ final class HomeTableViewCell: UITableViewCell {
   static let reuseIdentifier = String(describing: HomeTableViewCell.self)
   
   //MARK: - Views
-  private(set) var topicLabel: UILabel!
-  private(set) var shortDescriptionLabel: UILabel!
-  private(set) var writerLabel: UILabel!
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.distribution = .equalSpacing
+    return stackView
+  }()
+  
+  private lazy var topicLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .label
+    label.font = UIFont(
+      name: "BM YEONSUNG", size: 40
+    )
+    label.numberOfLines = 1
+    return label
+  }()
+  
+  private lazy var shortDescriptionLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .label
+    label.font = UIFont(
+      name: "BM YEONSUNG", size: 36
+    )
+    label.numberOfLines = 1
+    return label
+  }()
+  
+  private lazy var writerLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .label
+    label.font = UIFont(
+      name: "BM YEONSUNG", size: 20
+    )
+    label.numberOfLines = 1
+    label.textAlignment = .right
+    return label
+  }()
   
   //MARK: - Init
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+  override init(style: UITableViewCell.CellStyle,
+                reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     configureContentView()
-    configureTopicLabel()
-    configureShortDescriptionLabel()
-    configureWriterLabel()
+    setupStackView()
   }
   
   required init?(coder: NSCoder) {
@@ -25,12 +58,15 @@ final class HomeTableViewCell: UITableViewCell {
   
   override func layoutSubviews() {
     super.layoutSubviews()
-    let margins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    let margins = UIEdgeInsets(
+      top: 5, left: 5, bottom: 5, right: 5
+    )
     contentView.frame = contentView.frame.inset(by: margins)
   }
   
   override func prepareForReuse() {
     super.prepareForReuse()
+    topicLabel.text = nil
     shortDescriptionLabel.text = nil
     writerLabel.text = nil
   }
@@ -42,65 +78,6 @@ final class HomeTableViewCell: UITableViewCell {
     contentView.layer.masksToBounds = true
   }
   
-  private func configureTopicLabel() {
-    let padding: CGFloat = 8
-    topicLabel = UILabel()
-    contentView.addSubview(topicLabel)
-    
-    topicLabel.translatesAutoresizingMaskIntoConstraints = false
-    topicLabel.textColor = .label
-    topicLabel.font = UIFont(name: "BM YEONSUNG", size: 40)
-    topicLabel.numberOfLines = 1
-    topicLabel.text = "TOPIC"
-    
-    NSLayoutConstraint.activate([
-      topicLabel.topAnchor.constraint(equalTo: contentView.topAnchor,
-                                      constant: padding),
-      topicLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                          constant: padding),
-      topicLabel.heightAnchor.constraint(equalToConstant: 44),
-    ])
-  }
-  
-  private func configureShortDescriptionLabel() {
-    let padding: CGFloat = 8
-    
-    shortDescriptionLabel = UILabel()
-    contentView.addSubview(shortDescriptionLabel)
-    
-    shortDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-    shortDescriptionLabel.textColor = .label
-    shortDescriptionLabel.font = UIFont(name: "BM YEONSUNG", size: 36)
-    shortDescriptionLabel.numberOfLines = 1
-    shortDescriptionLabel.text = ""
-    
-    NSLayoutConstraint.activate([
-      shortDescriptionLabel.topAnchor.constraint(equalTo: topicLabel.bottomAnchor,
-                                                 constant: padding),
-      shortDescriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,
-                                                     constant: padding),
-      shortDescriptionLabel.heightAnchor.constraint(equalToConstant: 40),
-    ])
-  }
-  
-  private func configureWriterLabel() {
-    let padding: CGFloat = 8
-    
-    writerLabel = UILabel()
-    contentView.addSubview(writerLabel)
-    
-    writerLabel.translatesAutoresizingMaskIntoConstraints = false
-    writerLabel.textColor = .label
-    writerLabel.font = UIFont(name: "BM YEONSUNG", size: 20)
-    writerLabel.numberOfLines = 0
-    writerLabel.text = ""
-    
-    NSLayoutConstraint.activate([
-      writerLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-      writerLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding),
-    ])
-  }
-  
   func setCellData(shortDes: String, writer: String, topic: String) {
     DispatchQueue.main.async { [weak self] in
       guard let self = self else { return }
@@ -108,5 +85,29 @@ final class HomeTableViewCell: UITableViewCell {
       self.writerLabel.text = "-\(writer)-"
       self.topicLabel.text = topic
     }
+  }
+}
+
+extension HomeTableViewCell {
+  private func setupStackView() {
+    contentView.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    stackView.addArrangedSubview(topicLabel)
+    stackView.addArrangedSubview(shortDescriptionLabel)
+    stackView.addArrangedSubview(writerLabel)
+    
+    let margins = contentView.layoutMarginsGuide
+    
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(
+        equalTo: margins.topAnchor),
+      stackView.leadingAnchor.constraint(
+        equalTo: margins.leadingAnchor),
+      stackView.trailingAnchor.constraint(
+        equalTo: margins.trailingAnchor),
+      stackView.bottomAnchor.constraint(
+        equalTo: margins.bottomAnchor),
+    ])
   }
 }

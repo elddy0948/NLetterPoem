8274664923764f317a4a5ea4af153bae054f33a6
Joons <elddy0948@gmail.com>
2022-02-10 15:15:16 +0900

    [Refactor🛠] : SignInViewController Refactor

--- NLetterPoem/NLetterPoem/Screens/SignInSignUpScreen/Controllers/SignInViewController.swift
+++ NLetterPoem/NLetterPoem/Screens/SignInSignUpScreen/Controllers/SignInViewController.swift
@@ -1,60 +1,50 @@
 import UIKit
 import FirebaseAuth
 
+protocol SignInViewControllerDelegate: AnyObject {
+  func registerAction(
+    _ viewController: UIViewController
+  )
+  
+  func signInAction(
+    _ viewController: UIViewController
+  )
+}
+
 class SignInViewController: UIViewController {
   
   //MARK: - Views
-  private(set) var signinView: SignInView!
-  private(set) var tapGestureRecognizer: UITapGestureRecognizer!
+  private lazy var signinView = SignInView()
+  private lazy var tapGestureRecognizer: UITapGestureRecognizer = {
+    let tapGestureRecognizer = UITapGestureRecognizer(
+      target: self.view,
+      action: #selector(UIView.endEditing(_:))
+    )
+    return tapGestureRecognizer
+  }()
+  
+  //MARK: - Properties
+  weak var delegate: SignInViewControllerDelegate?
+  private var isUserSignin = false
+  
+  //MARK: - Initializer
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
   
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    configure()
-    configureSigninView()
-    configureTapGestureRecognizer()
+    setupViewController()
+    setupSubviews()
+    layout()
   }
   
-  private func configure() {
-    view.backgroundColor = .systemBackground
-    navigationController?.isNavigationBarHidden = true
-    NotificationCenter.default.addObserver(self,
-                                           selector: #selector(keyboardWillShown(notification:)),
-                                           name: UIResponder.keyboardWillShowNotification,
-                                           object: nil)
-    NotificationCenter.default.addObserver(self,
-                                           selector: #selector(keyboardWillDisappear(notification:)),
-                                           name: UIResponder.keyboardWillHideNotification,
-                                           object: nil)
-  }
-  
-  private func configureSigninView() {
-    let padding: CGFloat = 8
-    signinView = SignInView()
-    view.addSubview(signinView)
-    
-    signinView.delegate = self
-    
-    NSLayoutConstraint.activate([
-      signinView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      signinView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-      signinView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-      signinView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-    ])
-  }
-  
-  private func configureTapGestureRecognizer() {
-    tapGestureRecognizer = UITapGestureRecognizer(target: view,
-                                                  action: #selector(UIView.endEditing(_:)))
-    view.addGestureRecognizer(tapGestureRecognizer)
-  }
-  
-  private func showSignupViewController() {
-    let viewController = SignUpViewController()
-    viewController.modalPresentationStyle = .fullScreen
-    self.present(viewController, animated: true, completion: nil)
-  }
-  
+  //MARK: - Actions
   @objc private func keyboardWillShown(notification: Notification) {
     if view.frame.origin.y == 0 {
       view.frame.origin.y -= 50
@@ -68,27 +58,106 @@ class SignInViewController: UIViewController {
   }
 }
 
-
-extension SignInViewController: SigninViewDelegate {
-  func signinView(_ signinView: SignInView, didTapSignin button: NLPButton, email: String?, password: String?) {
-    guard let email = email,
-          let password = password else {
-      self.showAlert(title: "⚠️", message: "이메일과 패스워드를 다시 확인해주세요!", action: nil)
-      return
-    }
-    
-    AuthManager.shared.signin(email: email, password: password) { [weak self] error in
-      guard let self = self else { return }
-      if let error = error {
-        debugPrint(error)
-        self.showAlert(title: "⚠️", message: "로그인에 실패했어요!\n다시 시도해주세요!", action: nil)
-        return
-      }
-      self.navigationController?.popToRootViewController(animated: false)
-    }
+//MARK: - Setup ViewController
+extension SignInViewController {
+  private func setupViewController() {
+    view.backgroundColor = .systemBackground
+    view.addGestureRecognizer(tapGestureRecognizer)
+    setupNotification()
   }
   
-  func signinView(_ signinView: SignInView, didTapSignup button: NLPButton) {
-    showSignupViewController()
+  private func setupNotification() {
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillShown(notification:)),
+      name: UIResponder.keyboardWillShowNotification,
+      object: nil
+    )
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(keyboardWillDisappear(notification:)),
+      name: UIResponder.keyboardWillHideNotification,
+      object: nil
+    )
+  }
+}
+
+//MARK: - SigninViewDelegate
+extension SignInViewController: SigninViewDelegate {
+  func signinView(_ signinView: SignInView,
+                  didTapSignin button: NLPButton,
+                  email: String?,
+                  password: String?) {
+    guard let email = email,
+          let password = password else {
+            self.showAlert(
+              title: "⚠️",
+              message: "이메일과 패스워드를 다시 확인해주세요!",
+              action: nil
+            )
+            return
+          }
+    
+    signin(email: email, password: password)
+  }
+  
+  func signinView(
+    _ signinView: SignInView,
+    didTapSignup button: NLPButton
+  ) {
+    delegate?.registerAction(self)
+  }
+  
+  private func signin(email: String, password: String) {
+    AuthManager.shared.signin(
+      email: email,
+      password: password
+    ) { [weak self] error in
+      guard let self = self else { return }
+      defer {
+        if self.isUserSignin {
+          self.delegate?.signInAction(self)
+        }
+      }
+      if let error = error {
+        debugPrint(error)
+        self.showAlert(
+          title: "⚠️",
+          message: "로그인에 실패했어요!\n다시 시도해주세요!",
+          action: nil
+        )
+        self.isUserSignin = false
+        return
+      }
+      self.isUserSignin = true
+    }
+  }
+}
+
+//MARK: - UI Setup / Layout
+extension SignInViewController {
+  private func setupSubviews() {
+    view.addSubview(signinView)
+    signinView.delegate = self
+  }
+  
+  private func layout() {
+    let padding: CGFloat = 8
+    
+    NSLayoutConstraint.activate([
+      signinView.topAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.topAnchor
+      ),
+      signinView.leadingAnchor.constraint(
+        equalTo: view.leadingAnchor, constant: padding
+      ),
+      signinView.trailingAnchor.constraint(
+        equalTo: view.trailingAnchor, constant: -padding
+      ),
+      signinView.bottomAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.bottomAnchor
+      ),
+    ])
   }
 }

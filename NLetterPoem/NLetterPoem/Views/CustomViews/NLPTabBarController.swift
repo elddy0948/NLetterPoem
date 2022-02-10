32b8d8a285e4e92304a270d4653d615fbb6ba4f2
Joons <elddy0948@gmail.com>
2022-02-10 15:55:23 +0900

    [Refactor🛠] : Remove create ViewController logic

--- NLetterPoem/NLetterPoem/Views/CustomViews/NLPTabBarController.swift
+++ NLetterPoem/NLetterPoem/Views/CustomViews/NLPTabBarController.swift
@@ -20,45 +20,5 @@ class NLPTabBarController: UITabBarController {
     } else { }
 
     NLPTabBarController.tabBarTopAnchor = self.tabBar.topAnchor
-    
-    viewControllers = [createHomeNavigaionController(),
-                       createSearchNavigationController(),
-                       createRankingNavigationController(),
-                       createMyPageNavigationController()]
-  }
-  
-  private func createHomeNavigaionController() -> UINavigationController {
-    let viewController = HomeViewController()
-    let navController = UINavigationController(rootViewController: viewController)
-    navController.tabBarItem = UITabBarItem(title: nil, image: SFSymbols.houseFill, tag: 0)
-    navController.configureNavigationBarAppearance(.systemBackground)
-    return navController
-  }
-  
-  private func createSearchNavigationController() -> UINavigationController {
-    let viewController = SearchViewController()
-    let navController = UINavigationController(rootViewController: viewController)
-    viewController.title = "주제 검색"
-    navController.tabBarItem = UITabBarItem(title: nil, image: SFSymbols.magnifyingglass, tag: 1)
-    navController.configureNavigationBarAppearance(.systemBackground)
-    return navController
-  }
-  
-  private func createRankingNavigationController() -> UINavigationController {
-    let viewController = RankingViewController()
-    let navController = UINavigationController(rootViewController: viewController)
-    viewController.title = "N행시 달인"
-    navController.tabBarItem = UITabBarItem(title: nil, image: SFSymbols.crownFill, tag: 2)
-    navController.configureNavigationBarAppearance(.systemBackground)
-    return navController
-  }
-  
-  private func createMyPageNavigationController() -> UINavigationController {
-    let viewController = MyPageViewController()
-    let navController = UINavigationController(rootViewController: viewController)
-    viewController.title = "마이페이지"
-    navController.tabBarItem = UITabBarItem(title: nil, image: SFSymbols.personFill, tag: 3)
-    navController.configureNavigationBarAppearance(.systemBackground)
-    return navController
   }
 }

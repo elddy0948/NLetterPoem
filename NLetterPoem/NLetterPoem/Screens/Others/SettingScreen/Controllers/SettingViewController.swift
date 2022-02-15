8e0e652ab9a0038b3b170ec90dc2a9d5d6335e7d
Joons <elddy0948@gmail.com>
2022-02-16 01:06:46 +0900

    [Refactor🛠] : SettingViewController Refactor

--- NLetterPoem/NLetterPoem/Screens/Others/SettingScreen/Controllers/SettingViewController.swift
+++ NLetterPoem/NLetterPoem/Screens/Others/SettingScreen/Controllers/SettingViewController.swift
@@ -4,41 +4,14 @@ import Firebase
 class SettingViewController: UIViewController {
   
   //MARK: - Views
-  private(set) var settingTableView: SettingTableView!
+  private lazy var settingTableView = SettingTableView(frame: .zero, style: .plain)
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    configureSettingTableView()
     navigationItem.largeTitleDisplayMode = .never
     navigationItem.title = "설정"
-  }
-  
-  //MARK: - Privates
-  private func configureSettingTableView() {
-    settingTableView = SettingTableView(frame: .zero, style: .plain)
-    view.addSubview(settingTableView)
-    
-    settingTableView.delegate = self
-    settingTableView.dataSource = self
-    settingTableView.tableFooterView = UIView()
-    settingTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-    
-    NSLayoutConstraint.activate([
-      settingTableView.topAnchor.constraint(equalTo: view.topAnchor),
-      settingTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      settingTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      settingTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-    ])
-  }
-  
-  private func showLauchViewController() {
-    guard let scene = UIApplication.shared.connectedScenes.first,
-          let sceneDelegate = (scene.delegate as? SceneDelegate),
-          let coordinator = sceneDelegate.coordinator else {
-            return
-          }
-    
-    coordinator.present(animated: false, onDismissed: nil)
+    setupSettingTableView()
+    layout()
   }
   
   //MARK: - Section Type
@@ -67,57 +40,63 @@ extension SettingViewController {
       try Auth.auth().signOut()
       showLauchViewController()
     } catch {
-      debugPrint(error)
       showAlert(title: "⚠️", message: "로그아웃에 실패했습니다! 다시 시도해주세요!", action: nil)
     }
   }
   
   func withdrawalAction() {
-    let dispatchGroup = DispatchGroup()
-    var errorMessage: String?
-    var successCount = 0
-    
     guard let currentUser = Auth.auth().currentUser,
           let email = currentUser.email else { return }
     
-    dispatchGroup.enter()
-    DispatchQueue.global(qos: .utility).async {
-      AuthManager.shared.authDelete(userEmail: email) { result in
-        defer { dispatchGroup.leave() }
-        switch result {
-        case .success(_):
-          successCount += 1
-        case .failure(let error):
-          errorMessage = error.localizedDescription
-        }
-      }
+    DispatchQueue.global(qos: .utility).async { [weak self] in
+      guard let self = self else { return }
+      self.authDelete(with: email)
     }
+  }
+  
+  private func authDelete(with email: String) {
+    AuthManager.shared.authDelete(userEmail: email, completed: { result in
+      var flag = false
+      defer {
+        if flag { self.showLauchViewController() }
+      }
+      switch result {
+      case .success(let message):
+        flag = true
+        self.showAlert(title: "✅", message: message, action: nil)
+      case .failure(let error):
+        self.showAlert(title: "⚠️", message: error.localizedDescription, action: nil)
+      }
+    })
+  }
+
+  private func showLauchViewController() {
+    guard let scene = UIApplication.shared.connectedScenes.first,
+          let sceneDelegate = (scene.delegate as? SceneDelegate),
+          let coordinator = sceneDelegate.coordinator else {
+            return
+          }
     
-    dispatchGroup.enter()
-    DispatchQueue.global(qos: .utility).async {
-      UserDatabaseManager.shared.delete(email) { error in
-        defer { dispatchGroup.leave() }
-        if error != nil {
-          errorMessage = error?.localizedDescription
-          return
-        }
-      }
-    }
-    
-    dispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
-      guard let self = self else {
-        self?.showAlert(title: "⚠️", message: "삭제에 실패했습니다!", action: nil)
-        return
-      }
-      
-      if let errorMessage = errorMessage {
-        self.showAlert(title: "⚠️", message: errorMessage, action: nil)
-        return
-      }
-      
-      self.showAlert(title: "성공!", message: "삭제에 성공했습니다!", action: { _ in
-        self.showLauchViewController()
-      })
-    }
+    coordinator.present(animated: false, onDismissed: nil)
+  }
+}
+
+//MARK: - UI Setup / Layout
+extension SettingViewController {
+  private func setupSettingTableView() {
+    view.addSubview(settingTableView)
+    settingTableView.delegate = self
+    settingTableView.dataSource = self
+    settingTableView.tableFooterView = UIView()
+    settingTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+  }
+  
+  private func layout() {
+    NSLayoutConstraint.activate([
+      settingTableView.topAnchor.constraint(equalTo: view.topAnchor),
+      settingTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      settingTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      settingTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    ])
   }
 }
